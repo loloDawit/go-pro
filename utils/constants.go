70555f4de7f881bb04824a03165a5c1855f1c7d6
loloDawit <dawitabera64@gmail.com>
2024-07-08 14:52:39 -0700

    feat: Add constants for error messages
    The commit adds a new file `constants.go` in the `utils` package to define constants for error messages. This will provide a centralized location for managing error messages throughout the application.

---
+++ utils/constants.go
@@ -0,0 +1,13 @@
+package utils
+
+const (
+	ErrInvalidRequestBody  = "please send a valid request body"
+	ErrInvalidPayload      = "invalid payload"
+	ErrUserAlreadyExists   = "user with this email already exists"
+	ErrUserNotFound        = "user not found"
+	ErrHashingPassword     = "error hashing password"
+	ErrCreatingUser        = "error creating user"
+	ErrInternalServerError = "internal server error"
+	ErrUnauthorized        = "unauthorized"
+	ErrCreatingProduct     = "error creating product"
+)
