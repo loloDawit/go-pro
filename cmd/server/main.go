e9eaa900c298d7e5bbb58e387e83a151206594fd
loloDawit <dawitabera64@gmail.com>
2024-07-09 10:50:06 -0700

    chore: Refactor server initialization and routing
    This commit refactors the server initialization and routing logic. It includes the following changes:
    - Renames `cmd/main.go` to `cmd/server/main.go` for better organization.
    - Removes the `server` package and moves its functionality to the `APIServer` struct in `cmd/server/main.go`.
    - Updates the `main` function to use the `NewAPIServer` constructor and starts the server using the `Start` method.
    - Adds the necessary imports and initializes the router and subrouter.
    - Registers the user, product, and cart handlers with their respective routes.
    - Adds a log message to indicate the server is listening on the specified address.
    
    This refactoring improves the code structure and makes it easier to manage the server functionality.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,52 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/loloDawit/ecom/cmd/server"
 	"github.com/loloDawit/ecom/config"
 	"github.com/loloDawit/ecom/db"
+	"github.com/loloDawit/ecom/services/cart"
+	"github.com/loloDawit/ecom/services/order"
+	"github.com/loloDawit/ecom/services/product"
+	"github.com/loloDawit/ecom/services/user"
 )
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+	cfg  *config.Config
+}
+
+func NewAPIServer(addr string, db *sql.DB, cfg *config.Config) *APIServer {
+	return &APIServer{addr: addr, db: db, cfg: cfg}
+}
+
+func (s *APIServer) Start() error {
+	// initialize the router
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	// initialize the user handler
+	userHandler := user.NewHandlers(user.NewUserStore(s.db, s.cfg), s.cfg)
+	userHandler.RegisterRoutes(subrouter)
+
+	// initialize the product handler
+	productHandler := product.NewHandlers(product.NewProductStore(s.db, s.cfg))
+	productHandler.RegisterRoutes(subrouter)
+
+	// initialize the cart handler
+	orderStore := order.NewOrderStore(s.db, s.cfg)
+	cartHandler := cart.NewHandlers(orderStore, product.NewProductStore(s.db, s.cfg), s.cfg)
+	cartHandler.RegisterRoutes(subrouter)
+
+	// add log for server listening
+	log.Printf("Server is listening on %s", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
 
 func main() {
 	// Check current working directory
@@ -60,7 +99,7 @@ func main() {
 	initStorage(db)
 
 	// Initialize and start the server
-	server := server.NewAPIServer(cfg.Address, db, cfg)
+	server := NewAPIServer(cfg.Address, db, cfg)
 	if err := server.Start(); err != nil {
 		panic(err)
 	}
